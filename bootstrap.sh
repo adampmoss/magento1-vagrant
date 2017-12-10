671877cbb0765365ad885390f8cc73c375c5f45b
Adam Moss <adammoss7@gmail.com>
2017-12-10 19:09:09 +0000

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,64 @@
+# Update Apt
+# --------------------
+apt-get update
+
+# Install Apache & PHP
+# --------------------
+apt-get install -y apache2
+apt-get install -y php5
+apt-get install -y libapache2-mod-php5
+apt-get install -y php5-mysqlnd php5-curl php5-xdebug php5-gd php5-intl php-pear php5-imap php5-mcrypt php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl php-soap
+
+php5enmod mcrypt
+
+# Delete default apache web dir and symlink mounted vagrant dir from host machine
+# --------------------
+rm -rf /var/www/html
+mkdir /vagrant/httpdocs
+ln -fs /vagrant/httpdocs /var/www/html
+
+# Replace contents of default Apache vhost
+# --------------------
+VHOST=$(cat <<EOF
+NameVirtualHost *:8080
+Listen 8080
+<VirtualHost *:80>
+  DocumentRoot "/var/www/html"
+  ServerName localhost
+  <Directory "/var/www/html">
+    AllowOverride All
+  </Directory>
+</VirtualHost>
+<VirtualHost *:8080>
+  DocumentRoot "/var/www/html"
+  ServerName localhost
+  <Directory "/var/www/html">
+    AllowOverride All
+  </Directory>
+</VirtualHost>
+EOF
+)
+
+echo "$VHOST" > /etc/apache2/sites-enabled/000-default.conf
+
+a2enmod rewrite
+service apache2 restart
+
+# Mysql
+# --------------------
+# Ignore the post install questions
+export DEBIAN_FRONTEND=noninteractive
+# Install MySQL quietly
+apt-get -q -y install mysql-server-5.5
+
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS ledecolite2017"
+mysql -u root -e "GRANT ALL PRIVILEGES ON ledecolite2017.* TO 'magentouser'@'localhost' IDENTIFIED BY 'password'"
+mysql -u root -e "FLUSH PRIVILEGES"
+
+
+# Install n98-magerun
+# --------------------
+cd /vagrant/httpdocs
+wget https://raw.github.com/netz98/n98-magerun/master/n98-magerun.phar
+chmod +x ./n98-magerun.phar
+sudo mv ./n98-magerun.phar /usr/local/bin/
